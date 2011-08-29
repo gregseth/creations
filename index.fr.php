c0e9995e35c6e73b16dd4d95e3cdaaa41b7b89dd
gregseth <gregseth@342c2401-fae0-4cf2-9074-c435d3ab2b93>
2011-08-29 21:27:48 +0000

    .png
    git-svn-id: http://logrus.ath.cx/svn/gregseth.net@138 342c2401-fae0-4cf2-9074-c435d3ab2b93

--- index.fr.php
+++ index.fr.php
@@ -585,7 +585,7 @@
 
 
 	<p id="footer">
-		Sauf mention contraire, les créations présentes sur ce site sont sous licence <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.fr"><img src="img/cc-by-nc-sa" alt="CreativeCommons BY-NC-SA" /></a>. <br />Pour toute autre utilisation de ces images ou si vous avez des questions, contactez-moi&nbsp;: <a href="mailto:&#103;&#114;&#101;&#103;&#046;&#115;&#101;&#116;&#104;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;">greg.seth [at] gmail [dot] com</a>.
+		Sauf mention contraire, les créations présentes sur ce site sont sous licence <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.fr"><img src="img/cc-by-nc-sa.png" alt="CreativeCommons BY-NC-SA" /></a>. <br />Pour toute autre utilisation de ces images ou si vous avez des questions, contactez-moi&nbsp;: <a href="mailto:&#103;&#114;&#101;&#103;&#046;&#115;&#101;&#116;&#104;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;">greg.seth [at] gmail [dot] com</a>.
 	</p>
 
 </div><!--#description-->
